0d20f2250e1d170462f5aae4f93fa4170b1e85f5
iamadahn <vendonism16@yandex.ru>
2025-03-15 20:18:05 +0300

    tasks: servo: implemented common logic

--- src/tasks/servo.rs
+++ src/tasks/servo.rs
@@ -5,6 +5,8 @@ use embassy_sync::channel::Receiver;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use crate::data_types::{ServoInput, ServoDir};
 
+const SERVO_CENTER_DUTY: u16 = 1500;
+
 #[embassy_executor::task]
 pub async fn servo_controller_task(
     mut servo: SimplePwmChannel<'static, TIM2>,
@@ -12,5 +14,19 @@ pub async fn servo_controller_task(
     loop {
         let input = input_sub.receive().await;
         info!("received input for servo");
+
+        let duty: u16 = 50 * input.angle as u16 / 9;
+        match input.dir {
+            ServoDir::Left => {
+                servo.set_duty_cycle(SERVO_CENTER_DUTY - duty);
+            },
+            ServoDir::Right => {
+                servo.set_duty_cycle(SERVO_CENTER_DUTY + duty);
+            },
+            ServoDir::Middle => {
+                servo.set_duty_cycle(SERVO_CENTER_DUTY);
+            }
+        }
     }
 }
+
